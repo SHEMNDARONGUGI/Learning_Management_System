e1af5af7aea797e402329686996eb9302adc4487
SHEMNDARONGUGI <ndaroshem@gmail.com>
2024-12-22 22:35:15 +0300

    Initial commit

--- learning_management_app/urls.py
+++ learning_management_app/urls.py
@@ -18,6 +18,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', views.index, name="index" )
+    path('', views.index, name="index" ),
+    
 ]
